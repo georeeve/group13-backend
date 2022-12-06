d59af15d92cca63bfefa71cec04f8f8829ec05e6
George Reeve <george.fr.reeve@gmail.com>
2022-12-06 14:45:36 +0000

    Initial commit.

---
+++ src/main/java/com/example/group13backend/Group13BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.group13backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group13BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group13BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/group13backend/Group13BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group13backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group13BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
