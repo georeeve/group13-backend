cf68c32fa7f1caceb07ef9ef43d852e25d59f657
Ana Moros <ana.morosmarmol@sky.uk>
2022-12-07 16:03:29 +0000

    created getAllUsers endpoint

--- src/main/java/com/example/group13backend/controller/UserController.java
+++ src/main/java/com/example/group13backend/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.example.group13backend.controller;
 
+import com.example.group13backend.annotations.ApiMapping;
+import com.example.group13backend.db.models.Users;
+import com.example.group13backend.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@ApiMapping("users")
 public class UserController {
+    private final UserServices userServices;
+
+    @Autowired
+    public UserController(UserServices userServices) { this.userServices = userServices; }
+
+    @GetMapping
+    public List<Users> getUsers() { return userServices.getUsers(); }
 }
--- src/main/java/com/example/group13backend/services/UserServices.java
+++ src/main/java/com/example/group13backend/services/UserServices.java
@@ -1,4 +1,22 @@
 package com.example.group13backend.services;
 
+import com.example.group13backend.db.models.Users;
+import com.example.group13backend.db.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserServices {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServices(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<Users> getUsers() {
+        return userRepository.findAll();
+    }
 }
