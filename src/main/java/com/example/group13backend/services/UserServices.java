0ff6bf02baccecc45d07e7f26874d88736905587
Ana Moros <ana.morosmarmol@sky.uk>
2022-12-08 11:30:35 +0000

    added hashing to password

--- src/main/java/com/example/group13backend/services/UserServices.java
+++ src/main/java/com/example/group13backend/services/UserServices.java
@@ -2,19 +2,22 @@ package com.example.group13backend.services;
 
 import com.example.group13backend.db.models.Users;
 import com.example.group13backend.db.repository.UserRepository;
+import com.example.group13backend.utils.Argon2Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class UserServices {
     private final UserRepository userRepository;
-
+    private final Argon2Util argon2Util;
     @Autowired
-    public UserServices(UserRepository userRepository) {
+    public UserServices(UserRepository userRepository, Argon2Util argon2Util) {
         this.userRepository = userRepository;
+        this.argon2Util = argon2Util;
     }
 
     public List<Users> getAllUsers() {
@@ -29,11 +32,14 @@ public class UserServices {
         return userRepository.findById(userId);
     }
 
+
     public void addNewUser(Users users) {
         Optional<Users> usersOptional = userRepository.findUsersByEmail(users.getEmail());
         if(usersOptional.isPresent()) {
             throw new IllegalStateException("email taken");
         }
+        users.setPassword(argon2Util.hash(users.getPassword()));
+
         userRepository.save(users);
     }
 }
