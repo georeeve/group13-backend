9b1cd417e91010fea3f15385c06d3f5577c6f730
George Reeve <george.fr.reeve@gmail.com>
2022-12-08 11:26:08 +0000

    Add argon2 utils.

---
+++ src/main/java/com/example/group13backend/utils/Argon2Util.java
@@ -0,0 +1,14 @@
+package com.example.group13backend.utils;
+
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Argon2Util {
+
+    Argon2PasswordEncoder encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+
+    public String hash(String password) {
+        return encoder.encode(password);
+    }
+}
