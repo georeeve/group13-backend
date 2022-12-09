a1423cace34e506c4d6ab9befb724991b9177b96
georeeve <116298340+georeeve@users.noreply.github.com>
2022-12-09 11:01:52 +0000

    Add error statuses. (#3)

---
+++ src/main/java/com/example/group13backend/logging/Logger.java
@@ -0,0 +1,12 @@
+package com.example.group13backend.logging;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class Logger {
+
+    public void error(ErrorMessage errorMessage) {
+        throw new ResponseStatusException(errorMessage.status(), errorMessage.toString());
+    }
+}
