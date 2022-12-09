b594e0d7f9c5f6e763ef2b0062ab7cfbee4e7c88
Ana Moros <ana.morosmarmol@sky.uk>
2022-12-09 16:53:16 +0000

    added category and items db and seeded it

---
+++ src/main/java/com/example/group13backend/db/models/Category.java
@@ -0,0 +1,54 @@
+package com.example.group13backend.db.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table (name = "categories")
+public class Category {
+    @Id
+    @SequenceGenerator(
+            name = "categories_sequence",
+            sequenceName = "categories_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "categories_sequence"
+    )
+    private Long id;
+    private String Category;
+
+    public Category() {
+    }
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Item> items;
+
+    public Category(Long id, String category) {
+        this.id = id;
+        Category = category;
+    }
+
+    public Category(String category) {
+        Category = category;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getCategory() { return Category; }
+
+    public void setCategory(String category) { Category = category; }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", Category='" + Category + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/group13backend/db/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.group13backend.db.repository;
+
+import com.example.group13backend.db.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
