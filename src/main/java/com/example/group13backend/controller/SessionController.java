b4edc0834b2cf6c20482438414bccefa5c0c0aa4
georeeve <116298340+georeeve@users.noreply.github.com>
2022-12-12 16:41:43 +0000

    Change service mappings. (#17)

--- src/main/java/com/example/group13backend/controller/SessionController.java
+++ src/main/java/com/example/group13backend/controller/SessionController.java
@@ -2,7 +2,7 @@ package com.example.group13backend.controller;
 
 import com.example.group13backend.annotations.ApiMapping;
 import com.example.group13backend.db.models.User;
-import com.example.group13backend.services.UserServices;
+import com.example.group13backend.services.SessionService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +14,14 @@ import java.util.Map;
 @ApiMapping("session")
 public class SessionController {
 
-    private final UserServices userServices;
+    private final SessionService sessionService;
 
-    public SessionController(UserServices userServices) {
-        this.userServices = userServices;
+    public SessionController(SessionService sessionService) {
+        this.sessionService = sessionService;
     }
 
     @PostMapping
     public Map<String, String> newSession(@RequestBody User user) {
-        return Collections.singletonMap("token", userServices.newSession(user));
+        return Collections.singletonMap("token", sessionService.createSession(user));
     }
 }
