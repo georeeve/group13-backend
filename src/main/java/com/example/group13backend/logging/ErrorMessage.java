27837ed7f20d4e44ec78611bb8eb8e83786b4b34
georeeve <116298340+georeeve@users.noreply.github.com>
2022-12-12 16:06:45 +0000

    Change status codes. (#15)

--- src/main/java/com/example/group13backend/logging/ErrorMessage.java
+++ src/main/java/com/example/group13backend/logging/ErrorMessage.java
@@ -8,14 +8,14 @@ public enum ErrorMessage {
     EMAIL_INVALID(HttpStatus.BAD_REQUEST, "Not a valid email address"),
     NAME_INVALID(HttpStatus.BAD_REQUEST, "Not a valid first name or last name"),
     PASSWORD_TOO_SHORT(HttpStatus.BAD_REQUEST, "Password must be greater than 8 characters long"),
+    EMAIL_ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "User is already registered"),
+    DESCRIPTION_TOO_LONG(HttpStatus.BAD_REQUEST, "Description longer than 200 characters"),
+    QUANTITY_INVALID(HttpStatus.BAD_REQUEST, "Cannot make quantity less than 0"),
+    PRICE_INVALID(HttpStatus.BAD_REQUEST, "Cannot make price less than 0"),
     TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "Not a valid token"),
     USERNAME_OR_PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, "Username or password is incorrect"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User is not found"),
-    EMAIL_ALREADY_REGISTERED(HttpStatus.FORBIDDEN, "User is already registered"),
-    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "Item is not found"),
-    DESCRIPTION_TOO_LONG(HttpStatus.BAD_REQUEST, "Description longer than 200 characters"),
-    QUANTITY_INVALID(HttpStatus.BAD_REQUEST, "Cannot make quantity less than 0"),
-    PRICE_INVALID(HttpStatus.BAD_REQUEST, "Cannot make price less than 0");
+    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "Item is not found");
 
     private final HttpStatus statusCode;
     private final String message;
