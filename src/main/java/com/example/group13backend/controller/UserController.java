36480c178f94efe7b150ec40d953a25eba6f2bb1
georeeve <116298340+georeeve@users.noreply.github.com>
2022-12-12 16:34:44 +0000

    Update user route mappings. (#16)

--- src/main/java/com/example/group13backend/controller/UserController.java
+++ src/main/java/com/example/group13backend/controller/UserController.java
@@ -3,17 +3,14 @@ package com.example.group13backend.controller;
 import com.example.group13backend.annotations.ApiMapping;
 import com.example.group13backend.db.models.User;
 import com.example.group13backend.services.UserServices;
-import com.example.group13backend.views.PublicView;
-import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @RestController
-@ApiMapping("users")
+@ApiMapping("user")
 public class UserController {
     private final UserServices userServices;
 
@@ -22,44 +19,22 @@ public class UserController {
         this.userServices = userServices;
     }
 
-    @GetMapping
-    @JsonView(PublicView.class)
-    public List<User> getAllUsers() {
-        return userServices.getAllUsers();
+    @PostMapping
+    public Map<String, String> newUser(@RequestBody User user) {
+        return Collections.singletonMap("token", userServices.newUser(user));
     }
 
     @DeleteMapping
-    public void deleteCurrentUser(@RequestHeader("Authorization") String authorization) {
+    public void deleteUser(@RequestHeader("Authorization") String authorization) {
         final var user = userServices.getCurrentUser(authorization);
         userServices.deleteUserById(user.getId());
     }
 
-    @PostMapping(path = "/register")
-    public Map<String, String> registerUser(@RequestBody User user) {
-        return Collections.singletonMap("token", userServices.addNewUser(user));
-    }
-
-    @PostMapping(path = "/login")
-    public Map<String, String> loginUser(@RequestBody User user) {
-        return Collections.singletonMap("token", userServices.logInUser(user));
-    }
-
-    @GetMapping(path = "{userId}")
-    @JsonView(PublicView.class)
-    public User getUser(@PathVariable("userId") Long userId) {
-        return userServices.getUser(userId);
-    }
-
-    @DeleteMapping(path = "{userId}")
-    public void deleteUserById(@PathVariable("userId") Long id) {
-        userServices.deleteUserById(id);
-    }
-
-    @PutMapping(path = "{userId}")
-    public void updateUserById(
-            @PathVariable("userId") Long userId,
-            @RequestBody User newUser
-    ) {
-        userServices.updateUserById(userId, newUser);
+    @PatchMapping
+    public void patchUser(
+            @RequestHeader("Authorization") String authorization,
+            @RequestBody User newUser) {
+        final var user = userServices.getCurrentUser(authorization);
+        userServices.patchUserById(user.getId(), newUser);
     }
 }
