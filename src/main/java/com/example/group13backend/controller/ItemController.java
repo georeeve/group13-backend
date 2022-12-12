3f555497032caa2c4d71d0f9c03d9f821233e516
georeeve <116298340+georeeve@users.noreply.github.com>
2022-12-12 14:09:42 +0000

    Add items routes and updated error handling. (#10)
    * added PUT endpoint and error handling for items
    
    * updated put endpoint to use body; added more item errors
    
    Co-authored-by: Ana Moros <ana.morosmarmol@sky.uk>

--- src/main/java/com/example/group13backend/controller/ItemController.java
+++ src/main/java/com/example/group13backend/controller/ItemController.java
@@ -4,9 +4,7 @@ import com.example.group13backend.annotations.ApiMapping;
 import com.example.group13backend.db.models.Item;
 import com.example.group13backend.services.ItemServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,11 +15,35 @@ public class ItemController {
     private final ItemServices itemServices;
 
     @Autowired
-    public ItemController(ItemServices itemServices) { this.itemServices = itemServices; }
+    public ItemController(ItemServices itemServices) {
+        this.itemServices = itemServices;
+    }
 
     @GetMapping
-    public List<Item> getAllItems() { return  itemServices.geAllItems(); }
+    public List<Item> getAllItems() {
+        return itemServices.geAllItems();
+    }
 
     @GetMapping(path = "{itemId}")
-    public Optional<Item> getItemById(@PathVariable("itemId") Long itemId) { return itemServices.getItemById(itemId); }
+    public Item getItemById(@PathVariable("itemId") Long itemId) {
+        return itemServices.getItemById(itemId);
+    }
+
+    @PutMapping(path = "{itemId}")
+    public void updateItemById(
+            @PathVariable("itemId") Long itemId,
+            @RequestBody Item newItem
+    ) {
+        itemServices.updateItemById(itemId, newItem);
+    }
+
+//    public void updateItemById(
+//            @PathVariable("itemId") Long itemId,
+//            @RequestParam(required = false) String name,
+//            @RequestParam(required = false) String description,
+//            @RequestParam(required = false) Integer quantity,
+//            @RequestParam(required = false) Double price
+//    ) {
+//        itemServices.updateItemById(itemId, name, description, quantity, price);
+//    }
 }
--- src/main/java/com/example/group13backend/services/ItemServices.java
+++ src/main/java/com/example/group13backend/services/ItemServices.java
@@ -2,26 +2,88 @@ package com.example.group13backend.services;
 
 import com.example.group13backend.db.models.Item;
 import com.example.group13backend.db.repository.ItemRepository;
+import com.example.group13backend.logging.ErrorMessage;
+import com.example.group13backend.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class ItemServices {
     private final ItemRepository itemRepository;
+    private final Logger logger;
 
     @Autowired
-    public ItemServices(ItemRepository itemRepository) {
+    public ItemServices(ItemRepository itemRepository, Logger logger) {
         this.itemRepository = itemRepository;
+        this.logger = logger;
     }
 
     public List<Item> geAllItems() {
         return itemRepository.findAll();
     }
 
-    public Optional<Item> getItemById(Long itemId) {
-        return itemRepository.findById(itemId);
+    public Item getItemById(Long itemId) {
+        Optional<Item> item = itemRepository.findById(itemId);
+        if (item.isEmpty()) {
+            logger.error(ErrorMessage.ITEM_NOT_FOUND);
+            return null;
+        }
+        return item.get();
+    }
+
+    @Transactional
+    public void updateItemById(
+            Long itemId,
+            Item newItem
+    ) {
+        Optional<Item> itemOptional = itemRepository.findById(itemId);
+        if (itemOptional.isEmpty()) {
+            logger.error(ErrorMessage.ITEM_NOT_FOUND);
+            return;
+        }
+
+        Item oldItem = itemOptional.get();
+
+        if (newItem.getName() != null &&
+                newItem.getName().length() > 0 &&
+                !Objects.equals(oldItem.getName(), newItem.getName())
+        ) {
+            oldItem.setName(newItem.getName());
+        }
+
+        if (newItem.getDescription() != null &&
+                newItem.getDescription().length() > 0 &&
+                !Objects.equals(oldItem.getDescription(), newItem.getDescription())
+        ) {
+            if (newItem.getDescription().length() > 200) {
+                logger.error(ErrorMessage.DESCRIPTION_TOO_LONG);
+                return;
+            }
+            oldItem.setDescription(newItem.getDescription());
+        }
+
+        if (newItem.getQuantity() != null &&
+                !Objects.equals(oldItem.getQuantity(), newItem.getQuantity())
+        ) {
+            if (newItem.getQuantity() < 0) {
+                logger.error(ErrorMessage.QUANTITY_INVALID);
+            }
+            oldItem.setQuantity(newItem.getQuantity());
+        }
+
+        if (newItem.getPrice() != null &&
+                !Objects.equals(oldItem.getPrice(), newItem.getPrice())
+        ) {
+            if (newItem.getPrice() < 0) {
+                logger.error(ErrorMessage.PRICE_INVALID);
+            }
+            oldItem.setPrice(newItem.getPrice());
+        }
+
     }
 }
