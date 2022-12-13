e5c105c5efa331544d8ec4d8099c939cad299307
George Reeve <george.fr.reeve@gmail.com>
2022-12-13 10:38:36 +0000

    Update routes for tests.

--- src/test/java/com/example/group13backend/UserControllerTests.java
+++ src/test/java/com/example/group13backend/UserControllerTests.java
@@ -34,24 +34,17 @@ class UserControllerTests {
     }
 
     @Test
-    public void getAllUsers() {
-        final var response = this.restTemplate.getForObject("http://localhost:" + port + "/api/v1/users", User[].class);
-        assertThat(response).allMatch(user -> user.getId() != null && user.getEmail() != null && user.getPassword() != null
-            && user.getFirstName() != null && user.getLastName() != null && user.getDob() != null);
-    }
-
-    @Test
     public void newUsers() {
-        assertThat(postRegister(new User("Test", "Test", "test@example.com", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("token");
-        assertThat(postRegister(new User("Test", "Test", "test2@example.com", null, LocalDate.of(2022, JANUARY, 1)))).contains("Null value provided for required field");
-        assertThat(postRegister(new User("", "Test", "test3@example.com", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("Not a valid first name or last name");
-        assertThat(postRegister(new User("Test", "", "test4@example.com", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("Not a valid first name or last name");
-        assertThat(postRegister(new User("Test", "Test", "example", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("Not a valid email address");
-        assertThat(postRegister(new User("Test", "Test", "test5@example.com", "test123", LocalDate.of(2022, JANUARY, 1)))).contains("Password must be greater than 8 characters long");
-        assertThat(postRegister(new User("Test", "Test", "test@example.com", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("User is already registered");
+        assertThat(postNewUser(new User("Test", "Test", "test@example.com", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("token");
+        assertThat(postNewUser(new User("Test", "Test", "test2@example.com", null, LocalDate.of(2022, JANUARY, 1)))).contains("Null value provided for required field");
+        assertThat(postNewUser(new User("", "Test", "test3@example.com", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("Not a valid first name or last name");
+        assertThat(postNewUser(new User("Test", "", "test4@example.com", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("Not a valid first name or last name");
+        assertThat(postNewUser(new User("Test", "Test", "example", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("Not a valid email address");
+        assertThat(postNewUser(new User("Test", "Test", "test5@example.com", "test123", LocalDate.of(2022, JANUARY, 1)))).contains("Password must be greater than 8 characters long");
+        assertThat(postNewUser(new User("Test", "Test", "test@example.com", "testing123", LocalDate.of(2022, JANUARY, 1)))).contains("User is already registered");
     }
 
-    public String postRegister(User user) {
-        return this.restTemplate.postForObject("http://localhost:" + port + "/api/v1/users/register", user, String.class);
+    public String postNewUser(User user) {
+        return this.restTemplate.postForObject("http://localhost:" + port + "/api/v1/user", user, String.class);
     }
 }
