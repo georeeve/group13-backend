8cf1fed320ee9fc3cc2d765d757a4f8dd9b7dcd7
George Reeve <george.fr.reeve@gmail.com>
2022-12-15 12:13:35 +0000

    Test util functions

---
+++ src/test/java/com/example/group13backend/utils/Argon2UtilTest.java
@@ -0,0 +1,25 @@
+package com.example.group13backend.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class Argon2UtilTest {
+    @InjectMocks
+    private Argon2Util argon2Util;
+
+    @Test
+    public void hash() {
+        assertThat(argon2Util.hash("password").matches("\\$argon2id\\$v=.+")).isTrue();
+    }
+
+    @Test
+    public void verify() {
+        final var hash = argon2Util.hash("password");
+        assertThat(argon2Util.verify("password", hash)).isTrue();
+    }
+}
